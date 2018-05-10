a77d8b135e80158aa1627be5d21be079af0926e9
nasaplayer <nasaplayer@qq.com>
2018-05-10 21:05:04 +0800

    2.3php
    增加面向对象写法

--- 2.3.php
+++ 2.3.php
@@ -1,53 +1,60 @@
 <?php
-//（1）获得pt_local_token,也叫做pt_local_tk
-$token = getToken();
 
-//（2）获得当前登录QQ号
-$uin = pt_get_uins($token);
+$json ='';
 
-//（3）获得clientKey
-$clientkey = pt_get_st($uin,$token);
+$gc =186144206;
+$qq = new getCurrentQQ();
+$json = $qq->getGroupMembers($gc);
 
-//（4）获得QQ登录成功后的skey和richURL
-$arr = jump($token,$uin,$clientkey);
-$skey = $arr['skey'];
-$richURL = $arr['url'];
-
-//（5）获得QQ群页面登录成功后的p_skey
-$p_skey =getPSkey($richURL,$skey);
-
-//（6）凭借相关参数获取当前QQ的所在群列表数据
-$bkn = getBkn($skey);
-$Cookie = "Cookie: p_skey={$p_skey};uin=o{$uin} ;skey={$skey}";
-
-
-$json1 = getGroupList($bkn,$Cookie); 
-//显示当前登录QQ所在群列表数据
-echoJSON($json1);
-
-
-//$json2 = getGroupMembers($bkn,$Cookie,指定一个你所在的QQ群号);
-//显示当前登录QQ群成员列表
-//echoJSON($json2);
+$qq->echoJSON($json);
 
 
 
-function echoJSON($json){
-	echo '<pre>';
-	print_r( json_decode($json,false) );
-	echo '</pre>';
+class getCurrentQQ{
+	
+	protected $bkn='';
+	protected $Cookie='';
+	
+public function __construct(){
+	$token = $this->getToken();
+	
+	$uin = $this->pt_get_uins($token);
+	
+	$clientkey = $this->pt_get_st($uin,$token);
+	
+	$arr = $this->jump($token,$uin,$clientkey);
+	$skey = $arr['skey'];
+	$richURL = $arr['url'];
+	
+	$p_skey =$this->getPSkey($richURL,$skey);
+	$this->bkn = $this->getBkn($skey);
+	$this->Cookie = "Cookie: p_skey={$p_skey};uin=o{$uin} ;skey={$skey}";
+		
 }
 
-function getGroupList($bkn,$Cookie){
-	$url = 'https://qun.qq.com/cgi-bin/qun_mgr/get_group_list';
+
+public function getGroupMembers($gc){//$bkn,$Cookie,$gc
+	$bkn = $this->bkn;
+	$Cookie = $this->Cookie;
+	$url = 'https://qun.qq.com/cgi-bin/qun_mgr/search_group_members';
+	
+	//顺序也有关系
 	$post = [
-		'bkn'=>$bkn
+		'bkn'=>$bkn,
+		'end'=>4000,//结束数量
+		'gc'=>$gc, //群号
+		'sort'=>0,
+		'st' =>0,//起始数量
 	];
 	$header =[
 		$Cookie,
 	];
-	
-	$ch = curl_init();
+	$json = $this->oneCURL($url,$header,$post);
+	return $json;
+}
+
+
+public function oneCURL($url,$header,$post){
 	$optionArr = [
 		CURLOPT_URL=>$url,
 		CURLOPT_HEADER=>0,//将头文件的信息作为数据流输出
@@ -59,12 +66,34 @@ function getGroupList($bkn,$Cookie){
 	];
     $ch = curl_init();
 	curl_setopt_array ( $ch , $optionArr);
-	$json = curl_exec($ch); 
+	$json = curl_exec($ch);
 	
+	curl_close($ch);
 	return $json;
 }
 
-function charCodeAt($str, $index){
+public function echoJSON($json){
+	echo '<pre>';
+	print_r( json_decode($json,false) );
+	echo '</pre>';
+}
+
+public function getGroupList(){//$bkn,$Cookie
+	$bkn = $this->bkn;
+	$Cookie = $this->Cookie;
+	$url = 'https://qun.qq.com/cgi-bin/qun_mgr/get_group_list';
+	$post = [
+		'bkn'=>$bkn
+	];
+	$header =[
+		$Cookie,
+	];
+	
+	$json = $this->oneCURL($url,$header,$post);
+	return $json;
+}
+
+public function charCodeAt($str, $index){
     $char = mb_substr($str, $index, 1, 'UTF-8');
  	$value = null;
     if (mb_check_encoding($char, 'UTF-8')){
@@ -75,17 +104,17 @@ function charCodeAt($str, $index){
 }
 
 
-function getBkn($skey) {
+public function getBkn($skey) {
     $hash = 5381;
     for ($i = 0, $len = strlen($skey); $i < $len; ++$i){
-    	$hash +=($hash <<5) + charCodeAt($skey, $i);
+    	$hash +=($hash <<5) + $this->charCodeAt($skey, $i);
     	
     }
     return $hash & 2147483647;
 }
 
 
-function jump($token,$uin,$clientkey){
+public function jump($token,$uin,$clientkey){
 $url =<<<EOD
 https://ssl.ptlogin2.qq.com/jump?clientuin={$uin}&keyindex=9&pt_aid=715030901&daid=73&u1=https%3A%2F%2Fqun.qq.com%2Fmember.html%23&pt_local_tk={$token}&pt_3rd_aid=0&ptopt=1&style=40
 EOD;
@@ -126,9 +155,9 @@ $header =[
 	
 	$url = $matches[0];
 	
-	$skey =getCookie($header,'skey');
+	$skey =$this->getCookie($header,'skey');
 
-	tellMe(__FUNCTION__,$header,$body);
+	$this->tellMe(__FUNCTION__,$header,$body);
 	
 	
 	curl_close($ch);
@@ -142,7 +171,7 @@ $header =[
 }
 
 //ClientKey、clientuin
-function pt_get_st($uin,$token){
+public function pt_get_st($uin,$token){
 $url =<<<EOD
 https://localhost.ptlogin2.qq.com:4301/pt_get_st?clientuin={$uin}&callback=ptui_getst_CB&r=0.4266647630782271&pt_local_tk={$token}
 EOD;
@@ -178,16 +207,16 @@ $header =[
 	list($header, $body) = explode("\r\n\r\n", $content); 
 	// 解析COOKIE 
 	
-	$clientkey =getCookie($header,'clientkey');
+	$clientkey =$this->getCookie($header,'clientkey');
 	
-	tellMe(__FUNCTION__,$header,$body);
+	$this->tellMe(__FUNCTION__,$header,$body);
 
 	curl_close($ch);
 	
 	return $clientkey;	
 }
 
-function pt_get_uins($token){
+public function pt_get_uins($token){
 $url =<<<EOD
 https://localhost.ptlogin2.qq.com:4301/pt_get_uins?callback=ptui_getuins_CB&r=0.0760575656488639&pt_local_tk={$token}
 EOD;
@@ -231,12 +260,12 @@ $header =[
 	$user = $jsonObj[0];//使用第一个QQ快速登录
 	$uin = $user->uin;
 
-	tellMe(__FUNCTION__,$header,$body);
+	$this->tellMe(__FUNCTION__,$header,$body);
 	curl_close($ch);
 	return $uin;	
 }
 
-function getToken(){
+public function getToken(){
 $url =<<<EOD
 https://xui.ptlogin2.qq.com/cgi-bin/xlogin?appid=715030901&daid=73&hide_close_icon=1&pt_no_auth=1&s_url=https%3A%2F%2Fqun.qq.com%2Fmember.html%23
 EOD;
@@ -257,16 +286,16 @@ EOD;
 	
 	list($header, $body) = explode("\r\n\r\n", $content); 
 
-	$pt_local_token =getCookie($header,'pt_local_token');
+	$pt_local_token =$this->getCookie($header,'pt_local_token');
 	
 	
-	tellMe(__FUNCTION__,$header);
+	$this->tellMe(__FUNCTION__,$header);
 	curl_close($ch);
 	return $pt_local_token;
 }//end stepOne
 
 
-function getCookie($header,$name){
+public function getCookie($header,$name){
 	//"/pt_local_token=(.*?);/"
 	$rule = "/{$name}=(.*?);/";
 	preg_match($rule, $header, $matches); 
@@ -276,7 +305,7 @@ function getCookie($header,$name){
 }
 
 
-function tellMe($text,$header,$body=null){
+public function tellMe($text,$header,$body=null){
 
 echo '<div style="maring:10px;padding:5px;border:1px solid pink;overflow:hidden;">';
 	echo '<h3>'.$text.'响应头</h3>';
@@ -297,7 +326,7 @@ echo '</div>';
 }//end tellMe
 
 
-function getPSkey($richURL,$skey){
+public function getPSkey($richURL,$skey){
 	$optionArr = [
 		CURLOPT_URL=>$richURL,
 		CURLOPT_HEADER=>1,//将头文件的信息作为数据流输出
@@ -313,12 +342,13 @@ function getPSkey($richURL,$skey){
 	list($header, $body) = explode("\r\n\r\n", $content); 
 	// 解析COOKIE 
 	
-	$p_skey =getCookie($header,'p_skey');
+	$p_skey =$this->getCookie($header,'p_skey');
 	
 	
-	tellMe(__FUNCTION__,$header,$body);
+	$this->tellMe(__FUNCTION__,$header,$body);
 	curl_close($ch);
 	
 	return $p_skey;	
 }//end stepTwo
 
+}//getCurrentQQ
